3cf5519c72a5b48515ae570fb9af7aa3ac8b0e92
yuanchang <759337828@qq.com>
2020-07-06 12:24:53 +0800

    多步骤表单

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,7 @@ class Event < ApplicationRecord
   ranks :row_order
 
   has_many :tickets, :dependent => :destroy, :inverse_of  => :event
+  has_many :registrations, :dependent => :destroy
 
   accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
 
