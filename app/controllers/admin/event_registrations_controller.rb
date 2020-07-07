78efce3c2a1420526542a3ab5a7f90a69f12d6dd
yuanchang <759337828@qq.com>
2020-07-07 20:34:04 +0800

    资料筛选,多选

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -4,13 +4,22 @@ class Admin::EventRegistrationsController < AdminController
   def index
     @registrations = @event.registrations.includes(:ticket).order("id DESC").page(params[:page]).per(10)
 
-    if params[:status].present? && Registration::STATUS.include?(params[:status])
-      @registrations = @registrations.by_status(params[:status])
+    if Array(params[:statuses]).any?
+      @registrations = @registrations.by_status(params[:statuses])
     end
 
-    if params[:ticket_id].present?
-      @registrations = @registrations.by_ticket(params[:ticket_id])
+    if Array(params[:ticket_ids]).any?
+      @registrations = @registrations.by_ticket(params[:ticket_ids])
     end
+
+
+    # if params[:status].present? && Registration::STATUS.include?(params[:status])
+    #   @registrations = @registrations.by_status(params[:status])
+    # end
+    #
+    # if params[:ticket_id].present?
+    #   @registrations = @registrations.by_ticket(params[:ticket_id])
+    # end
   end
 
   def destroy
